e61d7f71e358e445cecba7e8cdadd7f97c62cead
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 23:47:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ActionController::Base
 
-  def index 
-  end 
+ def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/secrets/index'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
   
   def new 
   end 
