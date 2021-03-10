ef1203e9cdf4d154f0bc1d1bdd47fe5bc679db01
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 23:52:18 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,10 +1,17 @@
 class SecretsController < ApplicationController
   before_action :require_login
 
+  def current_user
+    session[:name] ||= nil
+  end
+  
   def show
   end
   
   def require_login 
-    if cu
+    if current_user.nil? || current_user.empty?
+  
+    else 
+      redirect_to controller: 'sessions', action 'new'
 
 end
